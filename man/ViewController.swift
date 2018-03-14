77e830c51ac313fd8ce0678e49bbaae0fbeab148
D7703_11 <d7703_11@d7703-11ui-imac.local>
2018-03-14 15:26:33 +0900

    color change

--- man/ViewController.swift
+++ man/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var sup = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
+        lab1.text = "레드"
     }
 
+    @IBOutlet weak var lab1: UILabel!
+    @IBAction func btn1(_ sender: Any) {
+        
+        if sup == 0 {
+            view.backgroundColor = UIColor.yellow
+            sup = sup + 1
+            lab1.text = "노랑"
+        }
+        
+        else if sup == 1 {
+            view.backgroundColor = UIColor.green
+            sup = sup + 1
+            lab1.text = "초록"
+        }
+        
+        else if sup == 2 {
+            view.backgroundColor = UIColor.darkGray
+            sup = sup - 2
+            lab1.text = "다크"
+        }
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
